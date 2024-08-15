647711628c98f1497b145ba8defcf88376a76dcb
Andi Azamuddin <andiazamuddin@gmail.com>
2024-08-15 17:59:06 +0800

    Update supabaseClient.ts

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://gqesvabbhirxyzmmyqkh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZXN2YWJiaGlyeHl6bW15cWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2ODk0NTEsImV4cCI6MjAzOTI2NTQ1MX0.87zm7uXPUJBLVZGihYz16E6qc2vMMlAr3QB73mq-3ts';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and anon key must be provided');
