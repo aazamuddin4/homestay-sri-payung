9919478ebbc989af0bca7a6a5cd9365545cb7a2e
Andi Azamuddin bin Andianwar <andiazamuddin@gmail.com>
2026-01-20 23:05:46 +0800

    Clarify image15 import as JPEG
    Updated import comment for image15 to reflect file type change.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,7 +20,7 @@ import image11 from '../assets/image11.jpg'
 import image12 from '../assets/image12.jpg'
 import image13 from '../assets/image13.jpg'
 import image14 from '../assets/image14.jpeg' //jpg to jpeg
-import image15 from '../assets/image15.jpeg'
+import image15 from '../assets/image15.jpeg' //jpg to jpeg
 import image16 from '../assets/image16.jpeg'
 import image17 from '../assets/image17.jpeg'
 import image18 from '../assets/image18.jpeg'
